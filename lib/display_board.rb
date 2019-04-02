ebd33a76b8d15862bf2131551068822ce21c0fc7
Nicole Rothenay <nicoler@iccreativeagency.com>
2019-04-02 15:12:24 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,8 +1,10 @@
 # Define display_board that accepts a board and prints
 # out the current state.
-
+board = ["   |   |   ", "-----------", "   |   |   ", "-----------", "   |   |   "]
 def display_board(board)
 puts "#{0}"
 puts "#{1}"
 puts "#{2}"
+puts "#{3}"
+puts "#{4}"
 end
