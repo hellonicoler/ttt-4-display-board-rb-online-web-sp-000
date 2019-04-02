f03900361f9b4b6876c710f4ab466b3dc5c1404b
Nicole Rothenay <nicoler@iccreativeagency.com>
2019-04-02 15:18:03 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,10 +1,10 @@
 # Define display_board that accepts a board and prints
 # out the current state.
-board = ["   |   |   ", "-----------", "   |   |   ", "-----------", "   |   |   "]
+
 def display_board(board)
-puts "#{board[0]}"
-puts "#{board[1]}"
-puts "#{board[2]}"
-puts "#{board[3]}"
-puts "#{board[4]}"
+puts " #{board[0]} "
+puts " #{board[1]} "
+puts " #{board[2]} "
+puts " #{board[3]} "
+puts " #{board[4]} "
 end
